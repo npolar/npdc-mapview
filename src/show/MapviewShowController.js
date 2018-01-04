3dea871f4fedb50283a6f199171678a05dd778ce
srldl <siri.uldal@gmail.com>
2018-01-04 12:34:56 +0100

    mapoptions obj works

--- src/show/MapviewShowController.js
+++ src/show/MapviewShowController.js
@@ -8,45 +8,29 @@ var MapviewShowController = function($controller, $routeParams,$scope, $q, Mapvi
   });
   $scope.resource = Mapview;
 
-  console.log("hei");
-
 
   //Show map in Antarctica
   $scope.mapOptions = {};
+  //$scope.mapOptions.initcoord = [-72.011389, 2.535];
+
 
   let show = function() {
 
-  	console.log($scope);
-  	console.log("vvvvvvv");
 
-    $scope.show().$promise.then((expedition) => {
+    $scope.show().$promise.then((mapview) => {
+
+      console.log($scope);
+
+      let coverage = [[[-72.011389, 2.535], [-72.011389, 2.535]]];
 
 
-/*      if (expedition.locations) {
-         let bounds = (expedition.locations).map((locations) => [[locations.south, locations.west], [locations.north, locations.east]]);
-         $scope.mapOptions.coverage = bounds;
-         $scope.mapOptions.geojson = "geojson";
-      }
+      let mapoptions = Object.assign({
+        geojson: "geojson",
+        coverage: coverage,
+        initcoord: [-72.011389, 2.535]
+      });
 
-      var pi = [];
-       //Convert from camelCase to human readable
-      for(var a=0; a<($scope.document.people).length; a++){
-            if ($scope.document.people[a].role === 'expedition/cruise leader'){
-                             pi.push($scope.document.people[a]);
-
-      }}
-      $scope.pi = pi;
-
-      //Convert from camelCase to human readable
-      for(var j=0; j<($scope.document.activity).length; j++){
-         $scope.document.activity[j].activity_type = convert($scope.document.activity[j].activity_type);
-      }
-
-      //Convert from camelCase to human readable
-      for(var i=0; i<($scope.document.people).length; i++){
-      	 for(var k=0; k<($scope.document.people[i].roles).length; k++){
-         $scope.document.people[i].roles[k] = convert($scope.document.people[i].roles[k]);
-      }}  */
+      $scope.mapOptions = mapoptions;
 
     });
 
