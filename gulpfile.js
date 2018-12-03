0e0b64bce5f8feb1bed42d6ac9663ade1d59dfea
srldl <siri.uldal@gmail.com>
2018-12-03 15:12:48 +0100

    new common version

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
 var npdcGulp = require('npdc-gulp');
 var config = npdcGulp.baseConfig;
-config.COMMON_VERSION = '4.11.2';
+config.COMMON_VERSION = '4.11.3';
 npdcGulp.loadAppTasks(gulp, config);
